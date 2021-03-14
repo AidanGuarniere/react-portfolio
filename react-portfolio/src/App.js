517b4ca23c871af9948f5b8d6c9e5e764fc90025
aidan <aidanguarniere@gmail.com>
2021-03-14 13:48:52 -0700

    Nav component and header

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import {useState} from 'react'
+import Nav from './components/Nav'
 
 function App() {
+  // define titles
+  const [titles] = useState([
+    {name: "about Me"},
+    {name: "portfolio"},
+    {name: "contact"},
+    {name: "resume"}, 
+
+  ])
+
+  const [currentTitle, setCurrentTitle] = useState(titles[0]);
+
+
   return (
-    <>
-    hello
-    </>
+    <main>
+      <Nav
+        titles={titles}
+        setCurrentTitle = {setCurrentTitle}
+        currentTitle={currentTitle}
+      ></Nav>
+    </main>
   );
 }
 
--- react-portfolio/src/components/Nav/index.js
+++ react-portfolio/src/components/Nav/index.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 
-function Nav() {
+function Nav({titles, currentTitle, setCurrentTitle}) {
+
+    useEffect(() => {
+        document.title = currentTitle.name
+    }, [currentTitle])
+
     return (
-        <div>
-            
-        </div>
+        <header>
+            <h1> Aidan Guarniere </h1>
+            <nav>
+                <ul>
+                    {titles.map((title) => (
+                        <li className = 
+                            {title.name}
+                            key={title.name}
+                        >
+                            <span
+                                onClick={() => {
+                                    setCurrentTitle(title)
+                                }}
+                            > {title.name}</span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
     )
 }
 
