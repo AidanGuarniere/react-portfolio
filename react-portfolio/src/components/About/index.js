5c2bf42966d1a62089ddc383bc1c24b70ed63d30
aidan <aidanguarniere@gmail.com>
2021-03-17 19:05:46 -0700

    load About with headshot and sample paragraph when currentTitle === about Me

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
+import headshot from "../../assets/img/headshot-1.jpg"
 
-function About({currentTitle}) {
-    return(
-    <div>
-        <h1>{currentTitle.name}</h1>
-    </div>
-    )
-
+function About({ currentTitle }) {
+  return (
+    <section>
+      <h1>{currentTitle.name}</h1>
+      <img
+        src={headshot}
+        alt="Aidan"
+      />
+      <p> lorem ipsum</p>
+    </section>
+  );
 }
 
-export default About
+export default About;
