44214c2d5c27e7e7645dd6a13f317c97ceedb4df
aidan <aidanguarniere@gmail.com>
2021-03-17 18:15:30 -0700

    conditional rendering based off of currentTitle state

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useState} from 'react'
 import Nav from './components/Nav'
+import About from './components/About'
 
 function App() {
   // define titles
@@ -14,15 +15,39 @@ function App() {
 
   const [currentTitle, setCurrentTitle] = useState(titles[0]);
 
-
   return (
-    <main>
+    <div>
       <Nav
         titles={titles}
         setCurrentTitle = {setCurrentTitle}
         currentTitle={currentTitle}
       ></Nav>
+    <main>
+      {currentTitle.name === "about Me" ? (
+      <About
+        currentTitle={currentTitle}
+      ></About>
+      ) :currentTitle.name === "portfolio" ?(
+        <div>
+          portfolio
+        </div>
+      ) :currentTitle.name === "contact" ?(
+        <div>
+          contact
+        </div>
+      ) :currentTitle.name === "resume" ?(
+        <div>
+          resume
+        </div>
+      ) : (
+        <div>
+          You're not supposed to be in here!
+        </div>
+      )
+
+      }
     </main>
+    </div>
   );
 }
 
---
+++ react-portfolio/src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About({currentTitle}) {
+    return(
+    <div>
+        <h1>{currentTitle.name}</h1>
+    </div>
+    )
+
+}
+
+export default About
