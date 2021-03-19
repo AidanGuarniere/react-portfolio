a7f12ae6f0af6cd3c40419bfdedcb52bea3ecb08
aidan <aidanguarniere@gmail.com>
2021-03-18 22:55:18 -0700

    better contact form

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -1,15 +1,23 @@
 function Contact(currentTitle) {
-    return (
-        <section>
-            <h1>{currentTitle.name}</h1>
-            <h3 className="input-field">Name:</h3>
-            <input></input>
-            <h3 className="input-field">Email Adress:</h3>
-            <input></input>
-            <h3 className="input-field">Message:</h3>
-            <input></input>
-        </section>
-    )
+  return (
+    <section>
+        <h1>{currentTitle.name}</h1>
+      <form
+        action="mailto:aidanguarniere@gmail.com"
+        method="post"
+        encType="text/plain"
+      >
+        Name:
+        <input type="text" name="name" placeholder="Please enter your name here"></input>
+        E-mail:
+        <input type="email" name="mail" placeholder="Please enter your email adress here"></input>
+        Message:
+        <input type="text" name="message" placeholder="Please enter your message here"></input>
+        <input type="submit" value="Send"></input>
+        <input type="reset" value="Reset"></input>
+      </form>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
