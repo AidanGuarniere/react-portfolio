b9c78b79d2c9f36dd19b2b81a3ad9ded9b1e9e29
aidan <aidanguarniere@gmail.com>
2021-03-20 17:35:14 -0700

    conditionally render project categories

--- react-portfolio/src/components/Portfolio/index.js
+++ react-portfolio/src/components/Portfolio/index.js
@@ -1,41 +1,38 @@
-import { useState } from "react"
+import { useState } from "react";
+import Project from "../Project";
 
-function Portfolio ({currentTitle}) {
-    const [categories] = useState([
-        {type: "full-stack"},
-        {type: "back-end"},
-        {type: "front-end"},
-    ])
+function Portfolio({ currentTitle }) {
+  const [categories] = useState([
+    { type: "full-stack" },
+    { type: "back-end" },
+    { type: "front-end" },
+  ]);
 
-    const [currentCategory, setCurrentCategory] = useState(categories[0])
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
-    return (
-        <section>
-            <Projects
-                categories = {categories}
-                currentCategory = {currentCategory}
-                setCurrentCategory = {setCurrentCategory}
-            />
-        </section>
-    )
-
+  return (
+    <section>
+      <div>
+        <ul>
+          {categories.map((category) => (
+            <li className={category.type} key={category.type}>
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                }}
+              >
+                {" "}
+                {category.type}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Project
+        currentCategory={currentCategory}
+      />
+    </section>
+  );
 }
 
-export default Portfolio
-
-{/* <div>
-<ul>
-        {categories.map((category) => (
-            <li className = 
-                {category.type}
-                key={category.type}
-            >
-                <span
-                    onClick={() => {
-                        setCurrentCategory(category)
-                    }}
-                > {category.type}</span>
-            </li>
-        ))}
-    </ul>
-</div> */}
+export default Portfolio;
--- react-portfolio/src/components/Project/index.js
+++ react-portfolio/src/components/Project/index.js
@@ -1,5 +1,17 @@
-import {useEffect} from "react"
+import { useEffect } from "react";
 
-function Project({categories, currentCategory, setCurrentCategory}) {
-    
+function Project({ currentCategory }) {
+  return (
+    <section>
+      {currentCategory.type === "full-stack" ? (
+        <p>FULL STACK</p>
+      ) :currentCategory.type === "back-end" ? (
+        <p>BACK END</p>
+      ) : (
+        <p>FRONT END</p>
+      )}
+    </section>
+  );
 }
+
+export default Project
